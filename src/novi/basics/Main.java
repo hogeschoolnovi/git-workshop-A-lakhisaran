4ed2a80053678f5323417e92fc1b9b404d2bf5ac
Amrish Lakhisaran <a.lakhisaran@novi-education.nl>
2020-06-02 11:35:05 +0200

    logincontroller

--- src/novi/basics/Main.java
+++ src/novi/basics/Main.java
@@ -3,6 +3,8 @@ package novi.basics;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello Dennis");
+
+        LoginController loginController = new LoginController();
     }
 }
